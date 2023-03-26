21f44116013f1ef1e87c786197f80d91501a0ce2
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2023-03-26 01:51:29 +0000

    attempted to implement .json file, could only log contents into console though

--- subpages/randomize.js
+++ subpages/randomize.js
@@ -1,31 +1,52 @@
-//obtain data from dataBoss.json file
-//this is all the data for world and weekly bosses
-function yank(objectName) {
+function yank(filter) {
+  //console.log(filter);
   
-}
+  var bosses = [];
+  
+  // disable CORS because path does not contain http(s)
+  fetch("./dataBoss.json", { mode: "no-cors" })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+      /*
+      //iterating through list
+      for (var i = 0; i < filter.length; i++) {
+        var key = filter[i];
+        console.log("kay: " + key);
+        console.log("data: " + data[key]);
+        bosses.push(data[key]);
+        console.log("bosses: " + bosses);
+      }
+      return bosses;
+    });*/
+  console.log("bosses: " + bosses);
+  
+  console.log("exited fetch");
 
-//sampling from list (adapted from stack overflow)
-function sample(this){
-  return this[Math.floor(Math.random() * this.length)];
+  return ['azhdaha'];
 }
-
 
 
 //randomizer functions
-//randomize bosses
-function randomizeBoss(filter, num=1){
-  var bossList = []; //list of bosses parsed from json
-  var randomized = []; //list to be returned
+
+function randomizeBoss(filter, num=1) {
+  var filteredBosses = yank(filter);
+  //searching times in set is better than in list
+  var bossList = new Set(filteredBosses);
+  var randomized = [];
+  
   while (randomized.length < num) {
-    var choice = bossList.sample;
-    bossList.splice(0, 1, choice);
-    randomized.push(choice);
-  return randomized;
+    var chosenIndex = Math.floor(Math.random() * bossList.size);
+    var chosenItem = Array.from(bossList)[chosenIndex];
+    if (!randomized.includes(chosenItem)){
+      randomized.push(chosenItem); 
+    }
   }
+  
+  document.getElementById("rBoss").innerHTML = "Selected Bosses: " + randomized.join(", ");
 }
 
 //randomize characters
-function randomizeCharacters(profiles, main){
+function randomizeCharacters(profiles, main) {
   if (profiles.length == 3) {
     
   }
