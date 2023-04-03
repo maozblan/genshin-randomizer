8ea2cd6e2f426dfc7082478914b1a14aa67a863f
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2023-04-03 16:42:04 -0700

    finished with boss randomizing filter basics :D

--- subpages/randomize.js
+++ subpages/randomize.js
@@ -6,11 +6,29 @@ let randomizeNum = 1;
 
 //toggle filters on and off
 function toggleButton(button) {
-  if (bossFilter.includes(button)) {
-    bossFilter.splice(bossFilter.indexOf(button), 1);
+  if (button == 'All') {
+    if (!bossFilter.includes('Weekly')) {
+      bossFilter.push('Weekly');
+    }
+    toggleButton('World');
   }
-  else {
-    bossFilter.push(button);
+  
+  let regions;
+  jsonData.Keys.forEach(item => {
+    if (item.name == button) {
+      regions = item.key;
+    }
+  });
+
+  for (let i = 0; i < regions.length; i++){
+    if (bossFilter.includes(regions[i])) {
+      if (button != 'World') {
+        bossFilter.splice(bossFilter.indexOf(regions[i]), 1);
+      }
+    }
+    else {
+      bossFilter.push(regions[i]);
+    }
   }
   document.getElementById('rFilter').innerHTML = "Current Filter: " + bossFilter.join(", ");
 }
@@ -19,13 +37,13 @@ function toggleButton(button) {
 function filterButtons() {
   const div = document.getElementById("bossFilter");
   
-  console.log('jsonData: ', jsonData);
+  //console.log('jsonData: ', jsonData);
   jsonData.Keys.forEach(item => {
     const button = document.createElement('button');
     button.textContent = item.name;
 
     button.addEventListener("click", () => {
-      console.log(item.name);
+      //console.log(item.name);
       toggleButton(item.name);
     });
   
@@ -53,12 +71,14 @@ function randomizeBoss() {
   let randomized = [];
   
   jsonData.Bosses.forEach(item => {
+    //console.log("current filter: ", bossFilter);
+    //console.log("current key: ", item.key);
+    //console.log(bossFilter.includes(item.key));
     if(bossFilter.includes(item.key)) {
-      bossData.concat(item.bosses);
+      bossData = bossData.concat(item.bosses);
     }
   });
-  
-  console.log(bossData);
+  //console.log(bossData);
 
   //to avoid overflow
   if (randomizeNum > bossData.length) {
@@ -69,6 +89,9 @@ function randomizeBoss() {
     let chosenIndex = Math.floor(Math.random() * bossData.length);
     randomized.push(bossData[chosenIndex]);
     bossData.splice(chosenIndex, 1);
+    //console.log("index: ", chosenIndex);
+    //console.log("randomized: ", randomized);
+    //console.log("bossData: ", bossData);
   }
 
   document.getElementById("rBoss").innerHTML = "Selected Bosses: " + randomized.join(", ");
