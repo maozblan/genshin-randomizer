590c39e7eb670d8785b3c0bcd5a0ab151ec613c2
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2023-04-05 14:57:01 -0700

    testing on profiles page, added character buttons

--- subpages/profiles.js
+++ subpages/profiles.js
@@ -1,6 +1,6 @@
 let jsonData;
-let output = document.getElementById('content').innerHTML;
-let choices = document.getElementById('choices').innerHTML;
+const output = document.getElementById('content');
+const choices = document.getElementById('choices');
 //output page type: main, add, edit
 
 //couple of other elements
@@ -30,22 +30,75 @@ function editProfile(fileName){
 
 
 function content(name) {
+    //grab localStorage profile data, then set up div 'content'
+    let data = JSON.parse(window.localStorage.getItem('genshinRandomizer'));
+
+    let profileList = document.createElement('div');
+    output.appendChild(profileList);
+
     if (name == 'main') {
         //load up page
-        jsonData.pyro.forEach(item => {
 
-        });
-    }
-    else {
-        //add version
-        if (name == 'edit') {
-            //load in the data of profile
+        // if jsonObj 'data' has key 'profiles'
+        if (!data[profiles]) {
+            let tempPara = document.createElement('p');
+            tempPara.textContent = "Head to 'Add' to create a new profile or upload an existing one!";
+            profileList.appendChild(tempPara);
         }
+        else {
+            data.profiles.forEach(item => {
+                let button = document.createElement('button');
+                let pfp = document.createElement('img');
+                let name = document.createElement('span');
+                
+                pfp.src = item.pfp;
+                name.textContent = item.name;
+
+                button.appendChild(pfp);
+                button.appendChild(name);
+
+                button.addEventListener("click", () => {
+                    //clicking pulls up the profile in edit mode
+                });
+
+                profileList.appendChild(button);
+            });
+        }
+
+    }
+    else if (name == 'add') {
+        //add version
+    }
+    else if (name == 'edit') {
+        //load in the data of profile
+    }
+    else if (name == 'testing') {
+        console.log('creating content, code: testing');
+
+        jsonData.chara.forEach(item => {
+            let button = document.createElement('button');
+            let pfp = document.createElement('img');
+            let chara = document.createElement('span');
+            pfp.src = chara.pfp;
+            chara = item.display;
+            button.textContent = item.display;
+
+            button.appendChild(pfp);
+            //button.appendChild(chara);
+
+            button.addEventListener("click", () => {
+                alert(chara + ' has been selected');
+            });
+
+            profileList.appendChild(button);
+        });
     }
 }
 
 
-async function initalizePage() {
+async function initializePage() {
+    console.log('initializing profiles page');
+
     const response = await fetch("./dataChara.json");
     jsonData = await response.json();
     
@@ -59,7 +112,11 @@ async function initalizePage() {
         choices.appendChild(button);
     }
 
-    content('main');
+    //using localStorage to store character profiles
+    //setting up base item
+    //window.localStorage.setItem('genshinRandomizer', '');
+
+    content('testing');
 };
 
 initializePage();
